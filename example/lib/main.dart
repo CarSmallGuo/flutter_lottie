3adb18f2e2a45b8dbdebc0437e9a4db3f6ff32b8
guoshuyu <359369982@qq.com>
2019-05-28 15:39:15 +0800

    fix

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,109 +11,108 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: [DefaultMaterialLocalizations.delegate],
+      home: Page(),
+    );
+  }
+}
 
-  LottieController controller;
+class Page extends StatefulWidget {
+  @override
+  _PageState createState() => _PageState();
+}
+
+class _PageState extends State<Page> {
   LottieController controller2;
+
   StreamController<double> newProgressStream;
 
-  _MyAppState() {
+  _PageState() {
     newProgressStream = new StreamController<double>();
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: PageDragger(
-        stream: this.newProgressStream,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Lottie'),
-          ),
-          body: Center(
-            child: Column(
-              children: <Widget>[
-                SizedBox(
+    return PageDragger(
+      stream: this.newProgressStream,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Lottie'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                child: Text("Play"),
+                onPressed: () {
+                  controller2.play();
+                },
+              ),
+              FlatButton(
+                child: Text("Stop"),
+                onPressed: () {
+                  controller2.stop();
+                },
+              ),
+              FlatButton(
+                child: Text("Pause"),
+                onPressed: () {
+                  controller2.pause();
+                },
+              ),
+              FlatButton(
+                child: Text("Resume"),
+                onPressed: () {
+                  controller2.resume();
+                },
+              ),
+              new Center(
+                child: SizedBox(
                   width: 150,
                   height: 150,
-                  child: LottieView.fromURL(
-                    url: "https://raw.githubusercontent.com/airbnb/lottie-ios/master/Example/Tests/Watermelon.json",
+                  child: LottieView.fromFile(
+                    filePath: "animations/newAnimation.json",
                     autoPlay: true,
                     loop: true,
                     reverse: true,
-                    onViewCreated: onViewCreated,
-                  )
-                ),
-                FlatButton(
-                  child: Text("Play"),
-                  onPressed: () {
-                    controller.play();
-                  },
-                ),
-                FlatButton(
-                  child: Text("Stop"),
-                  onPressed: () {
-                    controller.stop();
-                  },
-                ),
-                FlatButton(
-                  child: Text("Pause"),
-                  onPressed: () {
-                    controller.pause();
-                  },
-                ),
-                FlatButton(
-                  child: Text("Resume"),
-                  onPressed: () {
-                    controller.resume();
-                  },
-                ),
-                Text("From File"),
-                Container(
-                  child: SizedBox(
-                    width: 150,
-                    height: 150,
-                    child: LottieView.fromFile(
-                      filePath: "animations/newAnimation.json",
-                      autoPlay: true,
-                      loop: true,
-                      reverse: true,
-                      onViewCreated: onViewCreatedFile,
-                    ),
+                    onViewCreated: onViewCreatedFile,
                   ),
                 ),
-                FlatButton(
-                  child: Text("Change Color"),
-                  onPressed: () {
-                    // Set Color of KeyPath
-                    this.controller2.setValue(
-                      value: LOTColorValue.fromColor(
-                        color: Color.fromRGBO(0, 0, 255, 1)
-                      ),
-                      keyPath: "body Konturen.Gruppe 1.Fläche 1"
-                    );
-                    // Set Opacity of KeyPath
-                    this.controller2.setValue(
-                      value: LOTOpacityValue(opacity: 0.1),
-                      keyPath: "body Konturen.Gruppe 1.Fläche 1"
-                    );
-                  },
-                ),
-                Text("Drag anywhere to change animation progress"),
-              ],
-            ),
+              ),
+              FlatButton(
+                child: Text("Change Color"),
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) {
+                        return Scaffold(
+                          backgroundColor: Colors.transparent,
+                          body: new Center(
+                            child: SizedBox(
+                              width: 150,
+                              height: 150,
+                              child: LottieView.fromFile(
+                                filePath: "animations/newAnimation.json",
+                                autoPlay: true,
+                                loop: true,
+                                reverse: true,
+                                //onViewCreated: onViewCreatedFile,
+                              ),
+                            ),
+                          ),
+                        );
+                      });
+                },
+              ),
+              Text("Drag anywhere to change animation progress"),
+            ],
           ),
         ),
       ),
     );
-  }
-
-  void onViewCreated(LottieController controller) {
-    this.controller = controller;
-
-    // Listen for when the playback completes
-    this.controller.onPlayFinished.listen((bool animationFinished) {
-      print("Playback complete. Was Animation Finished? " + animationFinished.toString());
-    });
   }
 
   void onViewCreatedFile(LottieController controller) {
